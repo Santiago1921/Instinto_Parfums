94d9f5811713e572e48d78e698e431444a0a9df6
Santiago1921 <lunazentenosantiago2020@gmail.com>
2026-01-28 05:50:10 -0300

    Corrigiendo version de libreria3

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
+# 1. Instalar librerías
 pip install -r requirements.txt
 
+# 2. Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# 3. Crear las tablas de la base de datos (IMPORTANTE)
 python manage.py migrate
+
+# 4. Crear el usuario automáticamente (usando las variables que acabas de guardar)
+# El "|| true" es para que no falle si el usuario ya existe.
+python manage.py createsuperuser --noinput || true
