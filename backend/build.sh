81b98a17397763489a678e7e62c09c86f86a4d54
Santiago1921 <lunazentenosantiago2020@gmail.com>
2026-03-05 20:07:23 -0300

    fix deploy

--- backend/build.sh
+++ backend/build.sh
@@ -2,15 +2,11 @@
 # exit on error
 set -o errexit
 
-# 1. Instalar librerías
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
-# 2. Recolectar archivos estáticos
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-# 3. Crear las tablas de la base de datos (IMPORTANTE)
+echo "Running migrations..."
 python manage.py migrate
-
-# 4. Crear el usuario automáticamente (usando las variables que acabas de guardar)
-# El "|| true" es para que no falle si el usuario ya existe.
-python manage.py createsuperuser --noinput || true
